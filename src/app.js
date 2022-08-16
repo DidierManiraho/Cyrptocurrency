c38551cb0fe4bcbcb93d29f5de23c0c13676d9d2
Didier Maniraho <maniraho.didier2190@gmail.com>
2022-08-16 19:44:00 -0400

    javascript modufication

--- src/app.js
+++ src/app.js
@@ -1,77 +1,98 @@
-const formEl =   document.getElementById("form");
-const  buyerAmount = document.getElementById('buyer-amount');
-const currencyAgainst = document.getElementById('to-currency');
-const currencyFor = document.getElementById('from-currency');
-const submitBtn = document.getElementById('btn');
+const formEl = document.getElementById("form");
+var buyerQuantity = document.getElementById("buyer-amount");
+const btnswape = document.getElementById("btnswap");
 
-const currencyName = document.getElementById('currency-name');
-const currencyAmount = document.getElementById('currency-amount');
-
-const convertAmount = document.getElementById('convert-amount');
-const convertCurrency = document.getElementById('convert-currency');
-
-// Crypto currency against Dollar
-function dollarToCrypto(){
-    // get index of each element
-    let selectedOption2 = currencyFor.selectedIndex;
-    let selectedOption1 = currencyAgainst.selectedIndex;
-    // if the currency are the same
-    if(selectedOption1 === selectedOption2){
-        return buyerAmount.value * 1;
-    }
-
-    // from bitcoin to dollar
-    if(selectedOption2 === 0 && selectedOption1 === 2){
-        return  buyerAmount.value * 21000;
-    }
-    // From usd to bitcoin
-    if(selectedOption2 === 2 && selectedOption1 === 0){
-        return  buyerAmount.value / 21000;
-    }
-    // From Etherium to usd
-    if(selectedOption2 === 1 && selectedOption1 === 2){
-         return buyerAmount.value * 15000;
-    }
-    // From usd to etherium
-    if(selectedOption2 === 2 && selectedOption1 === 1){
-        return buyerAmount.value / 15000;
-    }
-}
-
-//1 Bitcoin (BTC) =12.80362037 Ethereum (ETH)
-// 1 Ethereum (ETH) =0.07810291 Bitcoin (BTC)
-//1 Ethereum (ETH) = 1,5000 United States Dollar "$" (USD)
-function EtherToCurrency(){
-    // get index of each element
-    let selectedOption2 = currencyFor.selectedIndex;
-    let selectedOption1 = currencyAgainst.selectedIndex;
-    // if the currency are the same
-    if(selectedOption1 === selectedOption2){
-        return buyerAmount.value * 1;
-    }
-
-    // from bitcoin to Etherium
-    if(selectedOption2 === 0 && selectedOption1 === 1){
-        return  buyerAmount.value * 12.80362037 ;
-    }
-    // From etherium to bitcoin
-    if(selectedOption2 === 1 && selectedOption1 === 0){
-        return  buyerAmount.value * 0.07810291 ;
-    }
-    // From Etherium to usd
-    if(selectedOption2 === 1 && selectedOption1 === 2){
-        return buyerAmount.value * 15000;
-    }
-    // From usd to etherium
-    if(selectedOption2 === 2 && selectedOption1 === 1){
-        return buyerAmount.value / 15000;
-    }
-}
+// results
+var fromCurrency = document.querySelector("#from-currency");
+var toCurrency = document.querySelector("#to-currency");
 
 
-formEl.addEventListener('change',function(){
-    currencyAmount.innerText = buyerAmount.value;
-    currencyName.innerText = currencyFor.value;
-    convertCurrency.innerHTML = currencyAgainst.value;
-    convertAmount.innerHTML = dollarToCrypto() || EtherToCurrency();
-})
+const sameCurrency = () => {
+    return buyerQuantity.value * 1;
+};
+
+// convert from ETH to USD vice versa
+const convertToETH = () => {
+    return Number(buyerQuantity.value) * 15000;
+};
+const convertFromETH = () => {
+    return Number(buyerQuantity.value) / 15000;
+};
+
+// convert from BTC to USD vice versa
+const convertToBTC = () => {
+    return Number(buyerQuantity.value) * 21000;
+};
+const convertFromBTC = () => {
+    return Number(buyerQuantity.value) / 21000;
+};
+
+/**
+ *
+ * @param fromCurrency
+ * @param toCurrency
+ * @param base
+ * @returns {number}
+ */
+//function convert(fromCurrency, toCurrency, base = 'USD'){
+const convert = (fromCurrencys, toCurrencys) => {
+    if (fromCurrencys === toCurrencys || toCurrencys === fromCurrencys)
+        return sameCurrency();
+
+    if (fromCurrencys === "ETH" && toCurrencys === "USD") return convertToETH();
+    if (fromCurrencys === "USD" && toCurrencys === "ETH") return convertFromETH();
+
+    if (fromCurrencys === "BTC" && toCurrencys === "USD") return convertToBTC();
+    if (fromCurrencys === "USD" && toCurrencys === "BTC") return convertFromBTC();
+};
+
+// swap button
+btnswape.addEventListener("click", function () {
+    let val1 =   fromCurrency.value;
+    let val2 =  toCurrency.value;
+
+
+    // Swap 2 value of the selection
+    [val1, val2] = [val2, val1];
+
+    fromCurrency.innerHTML = val1;
+    toCurrency.innerHTML = val2;
+
+
+    console.log(fromCurrency.innerHTML, toCurrency.value);
+});
+
+
+
+/*
+* document.getElementById("currency-amount")
+* document.getElementById("currency-name")
+*
+* document.getElementById("convert-amount")
+* document.getElementById("convert-currency")
+* */
+
+formEl.addEventListener("change", function () {
+    let result = convert(fromCurrency.value, toCurrency.value);
+    let display_currency="";
+    if(fromCurrency.value === "BTC" || toCurrency.value === "BTC")
+        display_currency = "Bitcoin (BTC)";
+    if(fromCurrency.value === "USD" || toCurrency.value === "USD")
+        display_currency = "States Dollar\"$\" (USD)";
+    if(fromCurrency.value === "ETH" || toCurrency.value === "ETH")
+        display_currency = "Ethereum (ETH)";
+    if(fromCurrency.value === toCurrency.value)
+        display_currency = fromCurrency.value || toCurrency.value;
+
+
+
+
+    document.getElementById("currency-amount").innerHTML=`${Number(buyerQuantity.value).toFixed(2)}`;
+    //document.getElementById("currency-name").innerHTML= fromCurrency.value;
+    document.getElementById("currency-name").innerHTML= display_currency;
+
+    //document.getElementById("convert-amount").innerHTML=toCurrency.value;
+    document.getElementById("convert-amount").innerHTML=display_currency;
+    document.getElementById("convert-currency").innerHTML=`${result}`;
+
+});
